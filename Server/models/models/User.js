50d151d94ad9a36a4ec5996eb2332167860b8fb5
Daniel Moretz <dcmoretz00@gmail.com>
2023-11-07 21:10:51 -0500

    I modified the user.js for the newuser.js to be seperate models

--- Server/models/models/User.js
+++ Server/models/models/User.js
@@ -17,18 +17,4 @@ const User = mongoose.model('User', userSchema);
 
 module.exports = User;
 
-const User = require('./models/user');
-
-const newUser = new User({
-  username: 'example_user',
-  password: 'hashed_password', // You should hash passwords for security
-});
-
-newUser.save((err, user) => {
-  if (err) {
-    console.error('Error saving user:', err);
-  } else {
-    console.log('User saved:', user);
-  }
-});
 
